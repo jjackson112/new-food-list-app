a19e05c7bdbd84861978c2db305dbf6b1a0bbf79
J. Jackson <jjackson11211@gmail.com>
2025-03-26 23:00:44 +0000

    ternary operator + onClick event

--- src/Food.js
+++ src/Food.js
@@ -3,17 +3,22 @@ import React, { useState } from "react";
 const Food = ({ name }) => {
   const [tried, setTried] = useState(false);
 
+  /*
   const onDelete = () => {
     props.deleteFood(props.food);
   };
+*/
 
   return (
-    <li>
+    <li className={tried ? "strike" : ""} onClick={() => setTried(!tried)}>
+      {name}
+    </li>
+    /*<li>
       {props.food}
       <span className="delete" onClick={onDelete}>
         X
       </span>
-    </li>
+    </li>*/
   );
 };
 
